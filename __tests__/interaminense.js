f5e55dad00da1624e99aed8d76da4de569c45813
Adriano Interaminense <adriano.interaminense@liferay.com>
2017-11-01 22:23:03 -0300

    Adds initial tests

---
+++ __tests__/interaminense.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import interaminense from '../src/interaminense';
+
+describe('interaminense', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
